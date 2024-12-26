474658db3397ee8cc72d150197bad9b5c18e8e3f
jbolanyos <jbolanyos@gmail.com>
2024-12-25 19:31:21 -0600

    actualizacion 25 de diciembre 19:31

--- pages/1_⚓_EDA.py
+++ pages/1_⚓_EDA.py
@@ -19,11 +19,16 @@ url='https://drive.google.com/file/d/1G19ESAC7JIz9ziGDayPh7p7ASJc7r3SB/view?usp=
 url='https://drive.google.com/uc?id=' + url.split('/')[-2]
 df = pd.read_csv(url)
 
+#def main()
+st.write(pd.head(5))
+
+
 #st.header("ANALISIS EXPLORATORIO")
 
-with st.container():
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.metric(label="Número de filas:", value=df.shape[0], border=True)
+#with st.container():
+#    col1, col2, col3 = st.columns(3)
+#    with col1:
+#        st.metric(label="Número de filas:", value=df.shape[0], border=True)
+
 
 #st.table(df.head(5))
